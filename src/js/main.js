bb556aa800f3b3ef070c92e6ce40bc2442fe8529
aguilarm <mika.aguilar@gmail.com>
2014-10-24 15:02:08 -0500

    UpdatePlayerSize() added, fixing bugs with search bar
    As I said last commit, there were some bugs.  Fixed everything I knew would break, whole thing works better now.

--- src/js/main.js
+++ src/js/main.js
@@ -29,22 +29,24 @@ function loadSearchBarButton() {
         var ytrSearchBarButtonSVG = chrome.extension.getURL('img/searchBarButton.svg');
         var ytrSearchBarButton = '<div id="ytrSearchBarButton"><img src="' + ytrSearchBarButtonSVG + '"></div>';
         $('#masthead-positioner-height-offset').after(ytrSearchBarButton);
-        $('#ytrSearchBarButton img').click(function () {
+        $('#ytrSearchBarButton').click(function () {
             console.log('clicked!');
             if (ytrSearchBarState === 1) {
-                document.getElementById('yt-masthead-container').style.display= "none";
-                document.getElementById('masthead-positioner').style.display= "none";
-                document.getElementById('masthead-positioner-height-offset').style.height = "0px";
-                document.getElementById('player').style.marginTop = "0px";
+                $('#yt-masthead-container').css('display', 'none');
+                $('#masthead-positioner').css('display', 'none');
+                $('#masthead-positioner-height-offset').css('height', '0px');
+                $('#player').css('margin-top', '0px');
                 $('#ytrSearchBarButton img').addClass('ytrRotate');
                 ytrSearchBarState = 0;
+                updatePlayerSize();
             } else {
                 $('#yt-masthead-container').css("display", "block");
                 $('#masthead-positioner').css('display', 'block');
                 $('#masthead-positioner-height-offset').css("height", "50px");
-                $('#player').css("marginTop", "10");
+                $('#player').css("margin-top", "10");
                 $('#ytrSearchBarButton img').removeClass('ytrRotate');
                 ytrSearchBarState = 1;
+                updatePlayerSize();
             }
         });
 }
@@ -80,19 +82,30 @@ function correctRelated() {
 //--------------------------------------
 //If the button has been pressed and the user resizes their window, update video player size
 //TODO Not working too well after youtube update
-$(window).resize(updatePlayerSize());
+$(window).resize(function () {updatePlayerSize()});
 
 //---------------------------------------
 //Function to update player size to adjust for changes
 function updatePlayerSize() {
-	if (resizeButtonState === 1){
+    console.log('updating player');
+	if (resizeButtonState === 1) {
 		//update window size
 		$window = $(window);
 		winH = $window.height();
-        
 		winW = $window.width();
-		$('#player-api').height(winH-50).width(winW);
-		$('#movie_player > div.html5-video-container > div.html5-video-content').height(winH - 50).width(winW - 50);
+        var winHadjusted;
+        if (ytrSearchBarState === 0) {
+		  winHadjusted = winH - 15;
+        } else {
+            winHadjusted = winH - 65;
+        }
+        console.log(winHadjusted);
+        $('#player-api').height(winHadjusted).width(winW);
+		$('#player').height(winHadjusted).width(winW);
+		//Also resize the video itself, sometimes does not stretch
+		$('video.html5-main-video').height(winHadjusted).width(winW);
+		$('#player-api').height(winHadjusted).width(winW);
+		$('#movie_player > div.html5-video-container > div.html5-video-content').height(winHadjusted).width(winW);
 	}
 }
 //---------------------------------------
@@ -115,27 +128,14 @@ function setStyle(cssText) {
 //The actual resize function
 function resizePlayer () {
 	//update window size
-	var progress,
-		$window = $(window),
-		winH = $window.height(),
-		winW = $window.width();
-		//adjust for youtube header
-        if (ytrSearchBarState === 0) {
-		  winHhead = winH - 65;
-        } else {
-            winHhead = winH -15;
-        }
+	var progress;
 	
 	if (resizeButtonState === 0){
 		//Make the progress bar stuff centered, a good enough way to solve the inabilty to scale the whole bar
 		//TODO Scale the whole bar, haha.  Problem is that it's being set with inline styles from a function and I can't
 		//quite navigate youtube's rather cryptic variables quite well enough to find where they grab the width value
 		setStyle('.ytp-progress-bar-container { width:854px;margin:auto;}',progress);
-		//Edit CSS so player fits screen properly
-		$('#player-api').height(winHhead).width(winW);
-		$('#player').height(winHhead).width(winW);
-		//Also resize the video itself, sometimes does not stretch
-		$('video.html5-main-video').height(winH-50).width(winW);
+        
 		$('#player-api').css("margin", 0);
 		$('#player').css("margin", 0);
 		$('#watch7-sidebar').attr("style", "margin-top:10px; top:0;position:absolute");
@@ -146,6 +146,8 @@ function resizePlayer () {
 		$('#ytr-resizeButton').css("background-image", "url(" + ytrResizeButtonSm + ")");
 		//Set resizeButtonState
 		resizeButtonState = 1;
+        
+        updatePlayerSize();
 		//Debug
 		//console.log("After button pressed and resizeButtonState is 0 found; " + resizeButtonState);
 	} else {
